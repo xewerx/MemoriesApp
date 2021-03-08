41103e43a71ff8b0fd64151723259c729be1f5c0
Ewaryst Ławecki <elawecki@candcpartners.local>
2021-03-08 20:11:10 +0100

    redux config

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import { useDispatch } from 'react-redux';
 
+import { getPosts } from './actions/posts';
 import Post from './components/Posts/Posts';
 import Form from './components/Form/Form';
 
@@ -9,6 +11,11 @@ import useStyles from './styles';
 
 function App() {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
 
   return (
     <Container maxwidth="lg">
@@ -27,7 +34,7 @@ function App() {
                     </Grid>
                 </Grid>
             </Container>
-        </Grow>
+          </Grow>
     </Container>
   );
 }
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
 import Post from './Post/Post';
 
 import useStyles from './styles';
 
 const Posts = () => {
+    const posts = useSelector((state) => state.posts)
+    // eslint-disable-next-line no-unused-vars
     const classes = useStyles();
     
+    console.log(posts);
     return (
         <>
         <h1>Post</h1>
